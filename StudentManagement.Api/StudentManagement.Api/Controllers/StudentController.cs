0d2e985e2dff09488b618311a7f3aae0d418bd04
biminvm <115773727+biminvm@users.noreply.github.com>
2023-06-13 14:14:02 +0530

    generics 1

--- StudentManagement.Api/StudentManagement.Api/Controllers/StudentController.cs
+++ StudentManagement.Api/StudentManagement.Api/Controllers/StudentController.cs
@@ -1,11 +1,88 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Api.Model;
+using StudentManagement.Repository;
 
-namespace StudentManagement.Api.Controllers
+
+[ApiController]
+[Route("[controller]")]
+public class StudentController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentController : ControllerBase
+    private readonly IRepository<Student> StudentRepository;
+
+    public StudentController(IRepository<Student> _studentRepository)
     {
+        StudentRepository = _studentRepository;
     }
+
+    /// <summary>
+    /// Endpoint to fetch details of an Students.
+    /// </summary>
+    [HttpGet]
+    public async Task<IActionResult> GetStudents()
+    {
+        var students = await StudentRepository.GetAll();
+        return Ok(students);
+    }
+
+    /// <summary>
+    /// Adding student data to the database
+    /// </summary>
+    /// <returns>
+    /// Id of inserted record
+    /// </returns>    
+    [HttpPost]
+    public async Task<IActionResult> AddStudent(Student student)
+    {
+        var createdStudent = await StudentRepository.Create(student);
+        return Ok(createdStudent.Id);
+    }
+
+    /// <summary>
+    /// Endpoint to delete a student by ID.
+    /// </summary>
+    /// <param name="id">students Id to fetch student's data</param>
+    [HttpDelete("student/{id}")]
+    public async Task<IActionResult> DeleteStudent(int id)
+    {
+        var deletedStudent = await StudentRepository.Delete(id);
+        if (deletedStudent == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(deletedStudent.Id);
+    }
+
+    /// <summary>
+    /// Endpoint to fetch details of an student with given id.
+    /// </summary>
+    /// <param name="id">Customers's Id to fetch student's data</param>
+    [HttpGet("student/{id}")]
+    public async Task<IActionResult> GetStudentById(int id)
+    {
+        var student = await StudentRepository.GetById(id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(student);
+    }
+
+    /// <summary>
+    /// Endpoint to update student record
+    /// </summary>
+    /// <param name="student">
+    /// customer contains the updated student's data
+    /// </param>
+    /// <returns> 
+    /// student id of updated record 
+    /// </returns>
+    [HttpPut]
+    public async Task<IActionResult> UpadteCustomer(Student student)
+    {
+        var updatedStudent = await StudentRepository.Update(student);
+        return Ok(updatedStudent.Id);
+    }
+
 }
---
+++ StudentManagement.Api/StudentManagement.Api/Repository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Api.Model;
+using StudentManagement.Api.Persistence;
+using StudentManagement.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly StudentDbContext dbContext;
+    private readonly DbSet<T> dbSet;
+
+    public Repository(StudentDbContext _dbContext)
+    {
+        this.dbContext = _dbContext;
+        this.dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAll()
+    {
+        return await dbSet.ToListAsync();
+    }
+
+    public async Task<T> GetById(int id)
+    {
+        var student = await dbSet.FindAsync(id);
+        if (student == null)
+            return null;
+        else
+            return student;
+    }
+
+    public async Task<T> Create(T entity)
+    {
+        dbSet.AddAsync(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> Delete(int id)
+    {
+        var entity = await dbSet.FindAsync(id);
+        if (entity == null)
+            return null;
+        dbSet.Remove(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> Update(T entity)
+    {
+        var idProperty = typeof(T).GetProperty("Id");
+        var entityId = idProperty?.GetValue(entity);
+        var existingEntity = await dbSet.FindAsync(entityId);
+        dbContext.Entry(existingEntity).CurrentValues.SetValues(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+    }
+}
